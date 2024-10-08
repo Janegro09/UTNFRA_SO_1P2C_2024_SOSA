c429dd6974363a61627683ece213248690567685
vboxuser <vboxuser@UTN-Debian.myguest.virtualbox.org>
2024-10-08 20:00:42 -0300

    FIX: punto D donde

--- RTA_SCRIPT_Examen_20241006/Punto_C_bis.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_C_bis.sh
@@ -2,7 +2,14 @@
 
 RUTA=$1
 
-# Ajustar permisos para las carpetas y su contenido
+#cambio permisos para poder escribir
+sudo chmod -R 775 $RUTA/* 
+
+sudo -u p1c2_2024_A1 bash -c "whoami > $RUTA/alumno_1/validar.txt"
+sudo -u p1c2_2024_A2 bash -c "whoami > $RUTA/alumno_2/validar.txt"
+sudo -u p1c2_2024_A3 bash -c "whoami > $RUTA/alumno_3/validar.txt"
+sudo -u p1c2_2024_P1 bash -c "whoami > $RUTA/profesores/validar.txt"
+
 # Permisos: rwx para el dueño, r-x para el grupo, --- para otros
 sudo chown -R p1c2_2024_A1:p1c2_2024_A1 "$RUTA/alumno_1"
 sudo chmod -R 750 "$RUTA/alumno_1"
@@ -19,8 +26,7 @@ sudo chmod -R 775 "$RUTA/alumno_3"
 sudo chown -R p1c2_2024_P1:p1c2_2024_gProfesores "$RUTA/profesores"
 sudo chmod -R 775 "$RUTA/profesores"
 
-sudo -u p1c2_2024_A1 bash -c "echo 'p1c2_2024_A1' > '$RUTA/alumno_1/validar.txt'"
-sudo -u p1c2_2024_A2 bash -c "echo 'p1c2_2024_A2' > '$RUTA/alumno_2/validar.txt'"
-sudo -u p1c2_2024_A3 bash -c "echo 'p1c2_2024_A3' > '$RUTA/alumno_3/validar.txt'"
-sudo -u p1c2_2024_P1 bash -c "echo 'p1c2_2024_P1' > '$RUTA/profesores/validar.txt'"
+
+#sudo -u p1c2_2024_A3 bash -c "echo 'p1c2_2024_A3' > '$RUTA/alumno_3/validar.txt'"
+#sudo -u p1c2_2024_P1 bash -c "echo 'p1c2_2024_P1' > '$RUTA/profesores/validar.txt'"
 
---
+++ RTA_SCRIPT_Examen_20241006/Punto_D.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo mkdir -p ../RTA_ARCHIVOS_Examen_20241006/Estructura_Asimetrica/{correo/{cartas_{1..100},cartero_{1..10}},clientes/cartas_{1..100}}
