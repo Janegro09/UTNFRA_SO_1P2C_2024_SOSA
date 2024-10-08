8fc3c6612936848e500f79f4b5c46f8a9d488c2b
janegro09 <javier.sosa099@gmail.com>
2024-10-08 03:28:57 -0300

    ADD: punto c done

---
+++ RTA_SCRIPT_Examen_20241006/Punto_C.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+LISTA=$1
+USER_HASH=$2
+HASH=$(sudo grep -i $USER_HASH /etc/shadow | awk -F ':' '{print $2}' )
+
+#echo "Clave:"
+#echo "$HASH"
+
+ANT_IFS=$IFS
+IFS=$'\n'
+for LINEA in `cat $LISTA |  grep -v ^#`
+do
+	echo "Linea: $LINEA"
+	echo
+	USUARIO=$(echo  $LINEA |awk -F ',' '{print $1}')
+	GRUPO=$(echo  $LINEA |awk -F ',' '{print $2}')
+	if [ $(grep $GRUPO -c /etc/group) == 0 ]; then
+		sudo groupadd $GRUPO
+		sudo useradd -m -p "$HASH" -s /bin/bash -G $GRUPO $USUARIO
+		#echo "------------------------"
+		#echo 
+	fi
+done
+IFS=$ANT_IFS
+
