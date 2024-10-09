b8b376f6025c562e35b5e9f34a37ac1e0c4d01db
vboxuser <vboxuser@UTN-Debian.myguest.virtualbox.org>
2024-10-09 10:16:20 -0300

    FIX: cambios en punto D

--- RTA_SCRIPT_Examen_20241006/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_D.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo mkdir -p ../RTA_ARCHIVOS_Examen_20241006/Estructura_Asimetrica/{correo/{cartas_{1..100},cartero_{1..10}},clientes/cartas_{1..100}}
+sudo mkdir -p /home/vboxuser/Estructura_Asimetrica/{correo/{cartas_{1..100},cartero_{1..10}},cliente/cartas_{1..100}}
